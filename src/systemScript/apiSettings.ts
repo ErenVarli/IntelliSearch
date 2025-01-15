8d0f7ee42c015a0875f1c144c0139bda283aac31
ErenVarli9 <eren.varli@epitech.eu>
2025-01-15 22:00:01 +0100

    [edit] changement texte

--- src/systemScript/apiSettings.ts
+++ src/systemScript/apiSettings.ts
@@ -1,5 +1,5 @@
 export function settingsSearchBar() {
-  const TEXT = "Start dorking now with the filter button on the right corner..";
+  const TEXT = "Use the top right filter to dorking.";
   const observer = new MutationObserver(() => {
     const searchBar = document.getElementById("gsc-i-id1") as HTMLInputElement;
     if (searchBar) {
