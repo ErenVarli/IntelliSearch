e36257af26946afcffcbde3e830ec1eb0bd8f253
ErenVarli9 <eren.varli@epitech.eu>
2025-01-15 21:46:39 +0100

    [fix] - correction affichage popup, hide ou show les resultats de recherches

--- src/actions/DisplayPopup.ts
+++ src/actions/DisplayPopup.ts
@@ -1,20 +1,26 @@
 import { displaySearchResults } from "./DisplaySearchResults";
 
-export function hidePopup() {
-  let popup = <HTMLInputElement>document.getElementById("t-search-window");
-  let introduction_text = <HTMLInputElement>(
-    document.getElementById("introduction-text")
-  );
-
-  introduction_text.style.display = "none";
-  popup.style.display = "none";
-
-  displaySearchResults(true);
-  return true;
-}
-
-export function showPopup() {
-  let popup = <HTMLInputElement>document.getElementById("t-search-window");
-  popup.style.display = "block";
-  displaySearchResults(false);
+export function displayPopup(display: boolean) {
+  if (display) {
+    let popup = <HTMLInputElement>document.getElementById("t-search-window");
+    if (popup != null) {
+      let introduction_text = <HTMLInputElement>(
+        document.getElementById("introduction-text")
+      );
+      introduction_text.style.display = "block";
+      popup.style.display = "block";
+    }
+    displaySearchResults(false);
+  } else {
+    let popup = <HTMLInputElement>document.getElementById("t-search-window");
+    if (popup != null) {
+      let introduction_text = <HTMLInputElement>(
+        document.getElementById("introduction-text")
+      );
+      introduction_text.style.display = "none";
+      popup.style.display = "none";
+      displaySearchResults(true);
+    }
+    displaySearchResults(true);
+  }
 }
